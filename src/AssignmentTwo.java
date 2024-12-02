cd386ecff4a7f375126cdd7040166a7a2a16de64
SunLijiaoClarissa <2923783519@qq.com>
2024-12-02 17:27:34 +0800

    Test

--- src/AssignmentTwo.java
+++ src/AssignmentTwo.java
@@ -1,6 +1,10 @@
 public class AssignmentTwo {
     public static void main(String[] args) {
-        System.out.println("Assignment 2 - Part 1 Initialized");
+        Employee operator = new Employee("John Doe", 30, "123-456-7890", "Ride Operator", 45000);
+    Ride rollerCoaster = new Ride("Extreme Roller Coaster", 20, operator);
+
+    System.out.println("Ride Name: " + rollerCoaster.getRideName());
+    System.out.println("Operator: " + rollerCoaster.getOperator().getName());
     }
     public void partThree() {}
     public void partFourA() {}
