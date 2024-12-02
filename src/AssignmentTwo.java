e2862ed43e2afab69dc6ee2ec1d057b5bdc3ca7d
SunLijiaoClarissa <2923783519@qq.com>
2024-12-02 17:22:18 +0800

    Create Class A2

---
+++ src/AssignmentTwo.java
@@ -0,0 +1,11 @@
+public class AssignmentTwo {
+    public static void main(String[] args) {
+        System.out.println("Assignment 2 - Part 1 Initialized");
+    }
+    public void partThree() {}
+    public void partFourA() {}
+    public void partFourB() {}
+    public void partFive() {}
+    public void partSix() {}
+    public void partSeven() {}
+}
