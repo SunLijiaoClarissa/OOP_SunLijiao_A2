9e083c69f32404479344362aefabd48ee5864cb4
SunLijiaoClarissa <2923783519@qq.com>
2024-12-02 17:23:27 +0800

    Create person class

---
+++ src/Person.java
@@ -0,0 +1,40 @@
+public class Person {
+    private String name;
+    private int age;
+    private String contactNumber;
+
+    // Default constructor
+    public Person() {}
+
+    // Parameterized constructor
+    public Person(String name, int age, String contactNumber) {
+        this.name = name;
+        this.age = age;
+        this.contactNumber = contactNumber;
+    }
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+}
