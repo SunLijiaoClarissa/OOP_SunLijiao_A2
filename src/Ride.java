3d2ec600228de5d6ccf7b38a9afe834b81f465d1
SunLijiaoClarissa <2923783519@qq.com>
2024-12-04 15:07:23 +0800

    Update ride and interface

--- src/Ride.java
+++ src/Ride.java
@@ -1,7 +1,15 @@
-public class Ride {
+import java.util.LinkedList;      // 导入 Queue 接口
+import java.util.Queue; // 导入 LinkedList 类（它实现了 Queue 接口）
+
+public class Ride implements RideInterface {
     private String rideName;
     private int capacity;
     private Employee rideOperator;
+
+    // 访客队列和游乐设施历史记录队列
+    // 将字段声明为 final
+    private final Queue<Visitor> waitingQueue = new LinkedList<>();
+    private final Queue<Visitor> rideHistory = new LinkedList<>();
 
     // Default constructor
     public Ride() {}
@@ -36,5 +44,82 @@ public class Ride {
 
     public void setRideOperator(Employee rideOperator) {
         this.rideOperator = rideOperator;
+    }
+
+    // 实现 RideInterface 中的方法
+
+    @Override
+    public void addVisitorToQueue(Visitor visitor) {
+        if (waitingQueue.size() < capacity) {
+            waitingQueue.add(visitor); // 使用 LinkedList 的 add 方法
+            System.out.println(visitor.getName() + " has been added to the queue for the ride: " + rideName);
+        } else {
+            System.out.println("The queue is full, " + visitor.getName() + " cannot be added.");
+        }
+    }
+
+    @Override
+    public void removeVisitorFromQueue(Visitor visitor) {
+        if (waitingQueue.contains(visitor)) {
+            waitingQueue.remove(visitor); // 使用 LinkedList 的 remove 方法
+            System.out.println(visitor.getName() + " has been removed from the queue.");
+        } else {
+            System.out.println(visitor.getName() + " is not in the queue.");
+        }
+    }
+
+    @Override
+    public void printQueue() {
+        if (waitingQueue.isEmpty()) {
+            System.out.println("No visitors are waiting in the queue.");
+        } else {
+            System.out.println("Visitors in the queue:");
+            for (Visitor visitor : waitingQueue) {
+                System.out.println(visitor.getName());
+            }
+        }
+    }
+
+    @Override
+    public void runOneCycle() {
+        if (!waitingQueue.isEmpty()) {
+            Visitor visitor = waitingQueue.poll(); // 使用 LinkedList 的 poll 方法（移除队列头部元素）
+            addVisitorToHistory(visitor);  // 将访客添加到历史记录
+            System.out.println(visitor.getName() + " has ridden the " + rideName + " ride.");
+        } else {
+            System.out.println("No visitors are waiting for the ride.");
+        }
+    }
+
+    @Override
+    public void addVisitorToHistory(Visitor visitor) {
+        rideHistory.add(visitor); // 使用 LinkedList 的 add 方法（将访客加入历史记录）
+        System.out.println(visitor.getName() + " has been added to the ride history.");
+    }
+
+    @Override
+    public void checkVisitorFromHistory(Visitor visitor) {
+        if (rideHistory.contains(visitor)) {
+            System.out.println(visitor.getName() + " is in the ride history.");
+        } else {
+            System.out.println(visitor.getName() + " has not ridden the " + rideName + " yet.");
+        }
+    }
+
+    @Override
+    public void numberOfVisitors() {
+        System.out.println("Number of visitors who have ridden the " + rideName + ": " + rideHistory.size());
+    }
+
+    @Override
+    public void printRideHistory() {
+        if (rideHistory.isEmpty()) {
+            System.out.println("No visitors have ridden the " + rideName + " yet.");
+        } else {
+            System.out.println("Ride history for " + rideName + ":");
+            for (Visitor visitor : rideHistory) {
+                System.out.println(visitor.getName());
+            }
+        }
     }
 }
--- src/RideInterface.java
+++ src/RideInterface.java
@@ -1,10 +1,11 @@
 public interface RideInterface {
-    void addVisitorToQueue(Visitor visitor);
-    void removeVisitorFromQueue(Visitor visitor);
-    void printQueue();
-    void runOneCycle();
-    void addVisitorToHistory(Visitor visitor);
-    void checkVisitorFromHistory(Visitor visitor);
-    void numberOfVisitors();
-    void printRideHistory();
+    void addVisitorToQueue(Visitor visitor);  // 将访客添加到队列中
+    void removeVisitorFromQueue(Visitor visitor);  // 从队列中移除访客
+    void printQueue();  // 打印队列
+    void runOneCycle();  // 执行一次游乐设施操作
+    void addVisitorToHistory(Visitor visitor);  // 将访客添加到游乐历史中
+    void checkVisitorFromHistory(Visitor visitor);  // 检查访客是否在游乐历史中
+    void numberOfVisitors();  // 获取游乐历史中的访客数量
+    void printRideHistory();  // 打印游乐历史
 }
+
